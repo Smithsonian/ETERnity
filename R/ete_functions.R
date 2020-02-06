f06ff60e701e8601551b912c13c4bfea910fc54f
Mike Trizna <triznam@si.edu>
2020-02-06 12:54:20 -0500

    Added ete_functions and an initial function

---
+++ R/ete_functions.R
@@ -0,0 +1,22 @@
+#' @title Extract site coordinates by provider
+#'
+#' @description This extracts the site coordinates by provider.
+#' @param data_tables Variable that holds the ete data tables
+#' @param provider_name Name of the dataset provider
+#'
+#' @return A dataframe of site coordinates from a given provider
+#'
+#' @examples
+#' \donttest{
+#'   wing_sites <- getlatlon('Wing')
+#' }
+#'
+#' @export
+
+getlatlon <- function(data_tables, provider_name) {
+  sites <- data_tables$dataset_table %>%
+                left_join(data_tables$sites_table, by="datasetname") %>%
+                filter(provider == provider_name) %>%
+                select(sitekey, sitename, latitude, longitude)
+  return(sites)
+}
